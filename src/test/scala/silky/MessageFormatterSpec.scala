860b5465331ead128613e9d6d8921c135fd61167
Franck Rasolo <franck.rasolo@gmail.com>
2014-04-15 17:06:05 +0100

    Left-padded expected audit messages with a margin

--- src/test/scala/silky/MessageFormatterSpec.scala
+++ src/test/scala/silky/MessageFormatterSpec.scala
@@ -13,15 +13,16 @@ class MessageFormatterSpec extends Spec with MustMatchers {
       val message = AuditMessage(from = "Foo", to = "Bar", timestamp = new Date(0L), id = "4babe38095", payload =
           <foo><bar>1</bar></foo>)
 
-      underTest.format(messageFlowId, message) mustBe s"""1970-01-01 00:00:00,000 Begin: 456
-Message-Id: 4babe38095
-From: Foo
-To: Bar
-
-<foo><bar>1</bar></foo>
-
-End: 456
-"""
+      underTest.format(messageFlowId, message) mustBe withoutMargin(s"""
+      |1970-01-01 00:00:00,000 Begin: 456
+      |Message-Id: 4babe38095
+      |From: Foo
+      |To: Bar
+      |
+      |<foo><bar>1</bar></foo>
+      |
+      |End: 456
+      |""")
     }
 
     def `be able to format an audit message with custom headers` {
@@ -29,17 +30,20 @@ End: 456
         .withHeader("Conversation", "d16dfac6-0896-4e38-aae2-13147fdee4be")
         .withHeader("Duration", "33 milliseconds")
 
-      underTest.format(messageFlowId, message) mustBe s"""1970-01-01 00:00:00,000 Begin: 456
-Message-Id: 4babe38095
-From: Foo
-To: Bar
-Conversation: d16dfac6-0896-4e38-aae2-13147fdee4be
-Duration: 33 milliseconds
-
-Hello World!
-
-End: 456
-"""
+      underTest.format(messageFlowId, message) mustBe withoutMargin(s"""
+      |1970-01-01 00:00:00,000 Begin: 456
+      |Message-Id: 4babe38095
+      |From: Foo
+      |To: Bar
+      |Conversation: d16dfac6-0896-4e38-aae2-13147fdee4be
+      |Duration: 33 milliseconds
+      |
+      |Hello World!
+      |
+      |End: 456
+      |""")
     }
   }
+
+  private def withoutMargin(text: String) = text.stripMargin.replaceFirst("\n", "")
 }
