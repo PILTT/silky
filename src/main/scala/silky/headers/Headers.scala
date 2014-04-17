33aeec7eaea5785d0c2ae4ff22528c7bc7dd31ca
Franck Rasolo <franck.rasolo@gmail.com>
2014-04-17 23:25:03 +0100

    Added empty parentheses to function evaluation

--- src/main/scala/silky/headers/Headers.scala
+++ src/main/scala/silky/headers/Headers.scala
@@ -11,7 +11,7 @@ object Headers {
   def scopedSet(messageFlowIdValue: String, unitOfWork: () => Unit) {
     try {
       MessageFlowId.set(Some(MessageFlowId(messageFlowIdValue)))
-      unitOfWork
+      unitOfWork()
     } finally {
       MessageFlowId.clear()
     }
