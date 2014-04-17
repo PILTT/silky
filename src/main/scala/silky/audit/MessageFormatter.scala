db619c68bb11ed8fe228c65dc40745240ab01e1b
Franck Rasolo <franck.rasolo@gmail.com>
2014-04-17 23:10:58 +0100

    Left-padded expected audit messages with a margin

--- src/main/scala/silky/audit/MessageFormatter.scala
+++ src/main/scala/silky/audit/MessageFormatter.scala
@@ -5,22 +5,27 @@ import java.text.SimpleDateFormat
 import java.util.TimeZone
 import scala.collection.Map
 import silky.MessageFlowId
+import silky.audit.MessageFormatter.withoutMargin
 
 class MessageFormatter(dateFormat: DateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS"),
                        timeZone: TimeZone = TimeZone.getTimeZone("UTC")) {
 
   dateFormat.setTimeZone(timeZone)
 
-  def format(messageFlowId: MessageFlowId, auditMessage: AuditMessage): String =
-    s"""${dateFormat.format(auditMessage.timestamp)} Begin: $messageFlowId
-${asLines(auditMessage.headers)}
-
-${auditMessage.payload}
-
-End: $messageFlowId
-""".toString
+  def format(messageFlowId: MessageFlowId, auditMessage: AuditMessage): String = withoutMargin(s"""
+    |${dateFormat.format(auditMessage.timestamp)} Begin: $messageFlowId
+    |${asLines(auditMessage.headers)}
+    |
+    |${auditMessage.payload}
+    |
+    |End: $messageFlowId
+    |""")
 
   private def asLines(headers: Map[String, String]) = headers
     .map(header => s"${header._1}: ${header._2}")
     .mkString("\n")
+}
+
+object MessageFormatter {
+  private[audit] def withoutMargin(text: String) = text.stripMargin.replaceFirst("\n", "")
 }
