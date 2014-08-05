5efec849bf76438d2500ab067f8523ed80cf8cbb
Franck Rasolo <franck.rasolo@gmail.com>
2014-08-05 21:29:27 +0100

    Corrected typo

--- src/main/scala/silky/audit/Slf4jMessageAuditor.scala
+++ src/main/scala/silky/audit/Slf4jMessageAuditor.scala
@@ -3,7 +3,7 @@ package silky.audit
 import org.slf4j.LoggerFactory.getLogger
 import silky.MessageFlowId
 
-class Sl4jMessageAuditor(defaultLoggerName: String, messageFormatter: MessageFormatter) extends MessageAuditor {
+class Slf4jMessageAuditor(defaultLoggerName: String, messageFormatter: MessageFormatter) extends MessageAuditor {
 
   def audit(messageFlowId: MessageFlowId, auditMessage: AuditMessage) {
     auditToNamedLog(defaultLoggerName, messageFlowId, auditMessage)
