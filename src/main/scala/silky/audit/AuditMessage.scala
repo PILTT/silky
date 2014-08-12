ec7cccb7954ab4f8dbd91c606a357b8270750ef1
Franck Rasolo <franck.rasolo@gmail.com>
2014-08-12 14:19:50 +0100

    Moved headers to AuditMessage

--- src/main/scala/silky/audit/AuditMessage.scala
+++ src/main/scala/silky/audit/AuditMessage.scala
@@ -1,16 +1,19 @@
 package silky.audit
 
+import Headers._
 import java.util.Date
 import java.util.UUID._
 import scala.collection.{Map, mutable}
-import silky.headers.Headers._
 
-case class AuditMessage(timestamp: Date = new Date,
-                        private val _headers: mutable.Map[String, String] = new mutable.LinkedHashMap[String, String],
-                        private val id: String = randomUUID.toString,
-                        private val from: String,
-                        private val to: String,
-                        payload: Any) {
+object Headers {
+  val MESSAGE_FLOW_ID = "Message-Flow-Id"
+  val MESSAGE_ID = "Message-Id"
+  val FROM = "From"
+  val TO = "To"
+}
+
+case class AuditMessage(id: String = randomUUID.toString, from: String, to: String, payload: Any, timestamp: Date = new Date) {
+  private val _headers: mutable.Map[String, String] = new mutable.LinkedHashMap[String, String]
 
   def withHeader(name: String, value: String): AuditMessage = {
     _headers += ((name, value))
